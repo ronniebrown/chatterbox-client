3bc285624cc8d23854ed43de27c62473a947a923
Ronnie Brown <ronnievoice@gmail.com>
2015-05-25 14:33:08 -0700

    initial

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,28 @@
 // YOUR CODE HERE:
+$(document).ready(function() {
+  
+  var getMessages = function(){
 
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: getMessages
+    }).appendTo('.chat');
+
+  };
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+});
