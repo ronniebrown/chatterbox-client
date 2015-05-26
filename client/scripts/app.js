057632008b9880851c98bec972f0469992a10b7f
Ronnie Brown <ronnievoice@gmail.com>
2015-05-25 19:58:33 -0700

    add escaped characters

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -16,6 +16,35 @@ App.prototype.template = function(user, msg, room){
   return message;
 };
 
+// escape special characters to prevent XSS
+App.prototype.escapeChars = function(str){
+  // var symbols = {
+  //   '&': '&amp;',
+  //   '<': '&lt;',
+  //   '>': '&gt;',
+  //   '"': '&quot;',
+  //   '/': '&#x2F;'
+  // };
+  return str.replace(/[.^$*+?()[{\\|\]-]/g, '\\$&');
+};
+
+App.prototype.showRooms = function(rooms){
+  for (var key in rooms) {
+    $('#rooms').append('<option value="' + key + '">' + key + '</option>');
+  }
+};
+
+// App.prototype.filterRooms = function(room){
+//   var tmp = $('p');
+//   $.each(tmp, function(){
+//     if (!$(this).find('span').hasClass(room)) {
+//       $(this).fadeOut();
+//     } else {
+//       $(this).fadeIn();
+//     }
+//   });
+// };
+
 App.prototype.onFetch = function(resultData) {
   $('.chat p').remove();
   var that = this;
@@ -23,12 +52,16 @@ App.prototype.onFetch = function(resultData) {
   resultData = _.sortBy(resultData, 'createdAt');
   _.each(resultData, function(item) {
     var room = $('<span>' + item.roomname + '</span>').text();
+    room = that.escapeChars(room);
     rooms[room] = room;
     var text = $('<span>' + item.text + '</span>').text();
+    text = that.escapeChars(text);
     var user = $('<span>' + item.username + '</span>').text();
+    user = that.escapeChars(user);
     var message = that.template(user, text, room);
     $('.chat').prepend(message);
   });
+  // that.showRooms(rooms);
 };
 
 App.prototype.fetch = function(){
@@ -42,6 +75,11 @@ App.prototype.fetch = function(){
       console.log('chatterbox: Message got');
       // data
       that.onFetch(data.results);
+      // if ($('#rooms').val() !== 'Main') {
+      //   that.filterRooms($('#rooms').val());
+      // } else {
+      //   $('p').show();
+      // }
     },
     error: function (data) {
       console.error('chatterbox: Failed to get message');
@@ -72,25 +110,27 @@ App.prototype.send = function(message){
   // allow users to create rooms
   // allow users to befriend each other
   // display messages from friends in bold
-  
-
 
 $(document).ready(function() {  
   var chat = new App();
   chat.fetch();
+  // chat.showRooms();
 
   // send message functionality
-  $('#send').on('click', function(){
+  $('#send').on('click', function(e){
+    e.preventDefault();
     var message = {
       'username': chat.username,
       'text': $('.textbox').val(),
       'roomname': ''
     };
     chat.send(message);
+    // return false;
   });
 
+  // continuous refresh
   // setInterval(function() {
   //   chat.fetch();
-  // }, 5000);
+  // }, 500);
   
 });
